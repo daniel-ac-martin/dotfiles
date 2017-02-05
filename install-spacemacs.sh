47a7911d83cb3deedab5f7f71986bafdfaa89bb4
Daniel A.C. Martin <github@daniel-martin.co.uk>
2017-02-05 21:28:15 +0000

    Spacemacs: Update installation script

--- install-spacemacs.sh
+++ install-spacemacs.sh
@@ -7,4 +7,6 @@ SCRIPT_DIR=$(dirname -- "$(readlink -e -- "${BASH_SOURCE}")")
 cd ~
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
 "${SCRIPT_DIR}/install-dotfile.sh" .spacemacs
+"${SCRIPT_DIR}/install-dotfile.sh" .emacs.d/private/dacm-keybindings/keybindings.el
+"${SCRIPT_DIR}/install-dotfile.sh" .emacs.d/private/dacm-keybindings/packages.el
 sudo dnf install -y -q emacs
