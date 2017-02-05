68bec3fa163ef655cef0dd18d0be4085f602c1a2
Daniel A.C. Martin <daniel-ac-martin@users.noreply.github.com>
2017-02-05 21:36:27 +0000

    Create directories when required

--- install-dotfile.sh
+++ install-dotfile.sh
@@ -3,8 +3,12 @@
 set -e
 
 FILE="${1}"
-SCRIPT_DIR=$(dirname -- "$(readlink -e -- "${BASH_SOURCE}")")
+DIR=$(dirname "${FILE}")
+
+mkdir -p "${DIR}"
+
 REPO_FILE=$(realpath --relative-to="${HOME}" "${FILE}")
+SCRIPT_DIR=$(dirname -- "$(readlink -e -- "${BASH_SOURCE}")")
 TARGET="${SCRIPT_DIR}/${REPO_FILE}"
 
 BASE_DIR=$(dirname "${FILE}")
