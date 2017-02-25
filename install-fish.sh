2254650ecd1c93bd89d756ec5454c2cbfb033462
Daniel A.C. Martin <daniel-ac-martin@users.noreply.github.com>
2017-02-25 18:36:37 +0000

    Fish: Bug-fix: Don't install defunct files

--- install-fish.sh
+++ install-fish.sh
@@ -8,8 +8,6 @@ LOGIN_FISH="/usr/local/bin/login-fish"
 cd ~
 "${SCRIPT_DIR}/install-dotfile.sh" .config/fish/config.fish
 "${SCRIPT_DIR}/install-dotfile.sh" .config/fish/functions/fish_prompt.fish
-"${SCRIPT_DIR}/install-dotfile.sh" .config/fish/functions/:q.fish
-"${SCRIPT_DIR}/install-dotfile.sh" .config/fish/functions/q.fish
 sudo dnf install -y fish
 sudo cp "${SCRIPT_DIR}/system-wide/${LOGIN_FISH}" "${LOGIN_FISH}"
 sudo sed -ri "s|^(${USER}:.*:).*$|\1${LOGIN_FISH}|" /etc/passwd
