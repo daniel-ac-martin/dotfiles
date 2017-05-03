6af5070a9c05bff7353bc7e734a0cc44fab6e447
Daniel A.C. Martin <github@daniel-martin.co.uk>
2017-05-03 16:53:35 +0100

    Fish: Improve and simplify installation

--- install-fish.sh
+++ install-fish.sh
@@ -7,7 +7,7 @@ LOGIN_FISH="/usr/local/bin/login-fish"
 
 cd ~
 "${SCRIPT_DIR}/install-dotfile.sh" .config/fish/config.fish
-"${SCRIPT_DIR}/install-dotfile.sh" .config/fish/functions/fish_prompt.fish
+"${SCRIPT_DIR}/install-dotfile.sh" .config/fish/functions
 sudo dnf install -y fish
 sudo cp "${SCRIPT_DIR}/system-wide/${LOGIN_FISH}" "${LOGIN_FISH}"
 sudo sed -ri "s|^(${USER}:.*:).*$|\1${LOGIN_FISH}|" /etc/passwd
