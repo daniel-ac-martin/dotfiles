ae86b34a279abc9f1bc4f588d89802e26ff56dc4
Daniel A.C. Martin <github@daniel-martin.co.uk>
2020-11-22 20:13:35 +0000

    Use BASH as main shell
    We can configure gnome-terminal to use fish instead.
    
    This is probably better as Fedora does not recognise the user as
    human when setting to shell to something odd.

--- install-fish.sh
+++ install-fish.sh
@@ -9,5 +9,7 @@ cd ~
 "${SCRIPT_DIR}/install-dotfile.sh" .config/fish/config.fish
 "${SCRIPT_DIR}/install-dotfile.sh" .config/fish/functions
 sudo dnf install -y fish
-sudo cp "${SCRIPT_DIR}/system-wide/${LOGIN_FISH}" "${LOGIN_FISH}"
-sudo sed -ri "s|^(${USER}:.*:).*$|\1${LOGIN_FISH}|" /etc/passwd
+
+# Uncomment to use as main shell (otherwise configure gnome-terminal)
+#sudo cp "${SCRIPT_DIR}/system-wide/${LOGIN_FISH}" "${LOGIN_FISH}"
+#sudo sed -ri "s|^(${USER}:.*:).*$|\1${LOGIN_FISH}|" /etc/passwd
