a93e31a78ac743ebfcac875c8654daeff11d410b
Daniel A.C. Martin <github@daniel-martin.co.uk>
2024-07-06 23:32:01 +0100

    GNOME: Allow right clicks with trackpad

--- configure-gnome.sh
+++ configure-gnome.sh
@@ -4,3 +4,6 @@ set -euo pipefail
 
 # Show the day of the week
 gsettings set org.gnome.desktop.interface clock-show-weekday true
+
+# Allow right clicks with trackpad
+gsettings set org.gnome.desktop.peripherals.touchpad click-method areas
