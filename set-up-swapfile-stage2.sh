3c144a2f269064be9755a490519530bb3a63d770
Daniel A.C. Martin <github@daniel-martin.co.uk>
2024-07-06 13:53:08 +0100

    Swap: Show helpful message earlier

--- set-up-swapfile-stage2.sh
+++ set-up-swapfile-stage2.sh
@@ -8,7 +8,6 @@ echo 'Placing SELinux into permissive mode.'
 sudo setenforce Permissive
 
 echo 'Hibernating the system in 10 seconds...'
+echo "When you have restored the system please run: ${stage3}"
 sleep 10
 sudo systemctl hibernate
-
-echo "When you have restored the system please run: ${stage3}"
