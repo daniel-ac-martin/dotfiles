0f3cfa26fe79cb2d6c98b53f91501efe489ba8c3
Daniel A.C. Martin <github@daniel-martin.co.uk>
2024-07-06 17:53:39 +0100

    Signal: Enable repo before using

--- install-signal.sh
+++ install-signal.sh
@@ -4,4 +4,5 @@ set -e
 
 # sudo dnf install -y flatpak # Already installed on Fedora
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak remote-modify --enable flathub
 flatpak install flathub org.signal.Signal
