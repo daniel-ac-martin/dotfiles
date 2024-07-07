6b7ea9f848bf3e6b91b2e3edf38c81782680d232
Daniel A.C. Martin <github@daniel-martin.co.uk>
2024-07-07 15:49:55 +0100

    Hibernate: Add support for auto-hibernation

--- set-up-swapfile-stage3.sh
+++ set-up-swapfile-stage3.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
+script_dir=$(dirname -- "$(readlink -e -- "${BASH_SOURCE}")")
+
 echo 'Tweaking SELinux rules...'
 sudo ausearch -c 'systemd-logind' --raw | audit2allow -M my-systemdlogind
 sudo ausearch -c 'systemd-sleep' --raw | audit2allow -M my-systemdsleep
@@ -12,5 +14,9 @@ echo '  Done.'
 
 echo 'Restoring SELinux enforcement.'
 sudo setenforce Enforcing
+
+echo 'Configuting auto-hibernate.'
+"${script_dir}/copy-system-file.sh" /etc/systemd/logind.conf
+"${script_dir}/copy-system-file.sh" /etc/systemd/sleep.conf
 
 echo 'You should now be able to hibernate your system at will.'
